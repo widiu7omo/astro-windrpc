9b03ccf136ee2ab91ec15136e2be5ec177149475
Danar Widi <widiu7omo@gmail.com>
2022-09-30 20:25:24 +0800

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 import WindiCSS from "astro-integration-windicss";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
