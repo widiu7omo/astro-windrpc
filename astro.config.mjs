c60c2ed8416b47602cc13aec1ddec7dd1891cc0d
Danar Widi <widiu7omo@gmail.com>
2022-09-30 20:20:25 +0800

    change adapter to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 import WindiCSS from "astro-integration-windicss";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
