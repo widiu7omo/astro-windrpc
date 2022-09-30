dd9a9e3f60373d2121944daa1d90307911de0218
Danar Widi <widiu7omo@gmail.com>
2022-09-30 17:50:55 +0800

    deployable to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
 import WindiCSS from "astro-integration-windicss";
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node(),
+  adapter: vercel(),
   integrations: [WindiCSS(), react()],
 });
